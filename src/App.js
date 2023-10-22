c00d424963a0ba39cf965ce6adafac6ffbb1a8f6
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-10-22 22:37:24 +0530

    made a to do list

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { TodoWrapper } from './components/TodoWrapper';  // Corrected the import statement
+
+function App() {
+  return (
+    
+    <div className='App'>
+      <TodoWrapper/>
+    </div>
+    
+  );
+}
+
+export default App;
